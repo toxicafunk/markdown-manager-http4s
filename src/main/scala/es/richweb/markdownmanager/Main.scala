03acef269866feecd75fc3bd497478088a1aa7c3
Eric Rodriguez <eric.rodriguez@eniro.com>
2020-03-26 22:33:08 +0100

    Clean project

--- src/main/scala/es/richweb/markdownmanager/Main.scala
+++ src/main/scala/es/richweb/markdownmanager/Main.scala
@@ -1,9 +1,71 @@
 package es.richweb.markdownmanager
 
-import cats.effect.{ExitCode, IO, IOApp}
+import cats.effect._
 import cats.implicits._
+import io.circe.Json
+import org.http4s.HttpRoutes
+import org.http4s.dsl.io._
+import org.http4s.implicits._
+import org.http4s.server.blaze._
+import io.circe.generic.auto._
+import io.circe.syntax._
+import org.http4s.circe._
+import org.http4s.client.blaze._
+import org.http4s.server.middleware._
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.global
 
 object Main extends IOApp {
-  def run(args: List[String]) =
-    Markdownmanagerhttp4sServer.stream[IO].compile.drain.as(ExitCode.Success)
+
+  val add10: Int => Int = i => i + 10
+  def times2: Int => Int = i => i * 2
+
+  def add10ThenTimes2(i: Int): Int = times2(add10(i))
+
+  val addThenMult: Int => Int = times2 compose add10
+
+  // OptionT[IO, Request, Response]
+  // Kleisli[Option[IO], Request, Response]
+
+  case class Hello(name: String)
+
+  private[markdownmanager] val baseurl = "https://confluence.eniro.com"
+  private[markdownmanager] val user = "user"
+  private[markdownmanager] val pass = "password"
+
+  val services = HttpRoutes
+    .of[IO] {
+      case GET -> Root / "hello" / name =>
+        Ok(Hello(s"hello $name").asJson)
+
+      case GET -> Root / "spaces" =>
+        BlazeClientBuilder[IO](global).resource.use { client =>
+          Ok(Spaces.get(client))
+        }
+
+      case req @ POST -> Root / "create" =>
+        BlazeClientBuilder[IO](global).resource.use { client =>
+          val ioJson = req.as[Json]
+          Ok(Spaces.post(client, ioJson))
+        }
+    }
+
+  val methodConfig = CORSConfig(
+    anyOrigin = false,
+    allowedOrigins = Set("http://localhost:3000"),
+    allowCredentials = false,
+    anyMethod = false,
+    allowedMethods = Some(Set("GET", "POST")),
+    maxAge = 3.day.toSeconds)
+
+  val corsService = CORS(services, methodConfig).orNotFound
+
+  def run(args: List[String]): IO[ExitCode] =
+    BlazeServerBuilder[IO]
+      .bindHttp(8080, "localhost")
+      .withHttpApp(corsService)
+      .serve
+      .compile
+      .drain
+      .as(ExitCode.Success)
 }
--- src/main/scala/es/richweb/markdownmanager/Spaces.scala
+++ src/main/scala/es/richweb/markdownmanager/Spaces.scala
@@ -3,25 +3,72 @@ package es.richweb.markdownmanager
 import org.http4s.client._
 import org.http4s.client.dsl.io._
 import org.http4s.headers._
-import org.http4s.MediaType
+import org.http4s.{BasicCredentials, MediaType, Request, Uri, UrlForm}
 import org.http4s.Method._
-import org.http4s.Uri
-import org.http4s.Credentials
-import org.http4s.AuthScheme
 import org.http4s.circe._
 import cats.effect.IO
 import io.circe.Json
+import io.circe.syntax._
 
 object Spaces {
 
+  val uri = Uri
+    .unsafeFromString(Main.baseurl)
+    .withPath("/rest/api/content/56000689")
+    .withQueryParams(
+      Map(
+        "type" -> "page",
+        "spaceKey" -> "DOC",
+        "expand" -> "body.storage"
+      )
+    )
+
   val request = GET(
-    Uri.unsafeFromString(Manager.baseurl),
-    Authorization(
-      Credentials.Token(AuthScheme.Basic, s"${Manager.user} ${Manager.pass}")
-    ),
+    uri,
+    Authorization(new BasicCredentials(Main.user, Main.pass)),
     Accept(MediaType.application.json)
   )
 
-  def get(client: Client[IO]) = client.expect[Json](request)
+  val postUri = Uri
+    .unsafeFromString(Main.baseurl)
+    .withPath("/rest/api/content")
+
+  val postRequest: (String, String) => IO[Request[IO]] = (title: String, body: String) => POST(
+    Json.obj(
+      ("type", Json.fromString("page")),
+      ("title", Json.fromString(title)),
+      ("space",
+        Json.obj(
+          ("key", Json.fromString("DOC"))
+        )
+      ),
+      ("body",
+        Json.obj(
+          ("storage",
+            Json.obj(
+              ("value", Json.fromString(body)),
+             ("representation", Json.fromString("storage"))
+            )
+          )
+        )
+      )
+    ),
+    postUri,
+    Authorization(new BasicCredentials(Main.user, Main.pass)),
+    Accept(MediaType.application.json)
+  )
+
+  def get(client: Client[IO]): IO[Json] = client.expect[Json](request).handleErrorWith(t => IO.pure(t.getMessage.asJson))
+
+  def post(client: Client[IO], ioJson: IO[Json]): IO[Json] = ioJson.flatMap(json => {
+    val cursor = json.hcursor
+    val title  = cursor.downField("mdname").as[String].fold[String](_ => "", s => s)
+    val body   = cursor.downField("typed").as[String].fold[String](_ => "", s => s)
+    client.expect[Json](postRequest(title, body)).map(rq => {
+      val resp = rq.as[Json].fold(df => df.message.asJson, j => j)
+      println(resp.noSpaces)
+      resp
+    }).handleErrorWith(t => IO.pure(t.getMessage.asJson))
+  })
 
 }
